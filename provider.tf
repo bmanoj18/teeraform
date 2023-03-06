9b860ec607465eef05e7ea6ad033d0db15407ddc
bmanoj18 <120033688+bmanoj18@users.noreply.github.com>
2023-03-06 21:45:05 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,4 +1,4 @@
 provider "google" {
-credentials = file("148225735457-compute@developer.gserviceaccount.com")
-project = var.fourth-arena-379102
+credentials = $file("Creditinals.json")
+project = My First Project
 }
