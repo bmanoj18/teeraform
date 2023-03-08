97f1ee436b0f5cdc3eb39eb08a708dd8061c13f8
bmanoj18 <120033688+bmanoj18@users.noreply.github.com>
2023-03-08 16:41:09 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,21 +2,26 @@ variable "fourth-arena-379102" {}
 variable "credentials.json" {}
 
 variable "name" {
+  type = string
   default = "test-vm"
 }
 
 variable "region" {
+  type = string
   default = "us-central1"
 }
   
 variable "zone" {
+  type = string
   default = "us-central1-a"
 }
 
 variable "machine_type" {
+  type = string
   default = "f1-micro"
 }
 
 variable "image" {
+  type = string
   default = "centos-7-v20230203"
 }
