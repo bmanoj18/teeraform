d15083a31e56b10b115baa3755ee093831f30e2c
bmanoj18 <120033688+bmanoj18@users.noreply.github.com>
2023-03-08 11:11:32 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "google" {
-credentials = "${file("credentials.json")}"
-project = "fourth-arena-379102"
+credentials = "${file("var.credentials")}"
+project = "${var.project}"
 region  = "us-central1"
 }
